b348add0e254215c70c1c4d814a39f1d63d248a4
ilyaprotsenk0 <iliafresfish@gmail.com>
2022-02-10 21:28:59 +0200

    setup webpack & other stuff

---
+++ app/main.519307cc3ecc6c890f62.js
@@ -0,0 +1,4 @@
+(() => {
+  "use strict";
+  for (let o = 0; o < 10; o++) console.log("Webpack works!");
+})();
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./styles/index.css";
+import "../index.html";
+
+for (let i = 0; i < 10; i++) {
+  console.log("Webpack works!");
+}
